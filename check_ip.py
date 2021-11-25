e21793d0a70ddc81b9a820eca35be399f4408751
JoYMnk <70647597+JoYMnk@users.noreply.github.com>
2021-11-25 22:46:47 +1345

    Create check_ip.py

---
+++ check_ip.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    'User-Agent': 'ArchebalT/888.0 (X66; Ybuntu; Linux x86_64; rv:94.0) Fraecko/20100101 Firefox/94.0'
+}
+
+def get_location(url):
+    response = requests.get(url=url, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+    
+    ip = soup.find('div', class_='ip').text.strip()
+    location = soup.find('div', class_='value-country').text.strip()
+    
+    print(f'IP: {ip}\nLocation: {location}')
+
+
+def main():
+    get_location(url='https://2ip.ru')
+    
+    
+if __name__ == '__main__':
+    main()
